ab5b83fb56ec163dfc55033a6fb31020fc827c86
Kim Hoon <71738968+kimoony@users.noreply.github.com>
2021-12-28 02:02:26 +0900

    nwitter update_01

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,9 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react';
+import authService from 'fBase';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from 'firebase/auth';
+
 
 function Auth() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
+
+  const onChange = (e) => {
+    const {
+      target: { name, value }
+    } = e;
+    // console.log(name, value);
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  }
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        data = await createUserWithEmailAndPassword(
+          authService,
+          email,
+          password
+        );
+      } else {
+        data = await signInWithEmailAndPassword(
+          authService,
+          email,
+          password
+        );
+      }
+      console.log(data);
+    } catch (err) {
+      console.error(err.message);
+      setError(err.message);
+    }
+  };
+
+  const toggleAccount = () => setNewAccount(prev => !prev);
+
   return (
     <div>
-      Auth
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          minLength={8}
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value={newAccount ? "Create Account" : "Sign In"} />
+        {error}
+      </form>
+      <span onClick={toggleAccount}>{newAccount ? "Sign In" : "Create Account"}</span>
+      <div>
+        <button>구글 로그인</button>
+        <button>깃허브 로그인</button>
+      </div>
     </div>
   )
 }
